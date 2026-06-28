2e7893d703b0dc0aa415f674d4abde38ecfe7de5
Hector <hhector7722@gmail.com>
2026-06-28 04:27:35 +0200

    ca

--- components/predictions/PredictionsCalendar.tsx
+++ components/predictions/PredictionsCalendar.tsx
@@ -533,6 +533,16 @@ export function PredictionsCalendar({
         </div>
       </section>
 
+      {!isJune && (
+        <div className="relative flex min-h-0 flex-1 flex-col overflow-hidden">
+          <KnockoutBracket
+            poolId={poolId}
+            matches={localMatches}
+            currentProfileId={currentProfileId}
+          />
+        </div>
+      )}
+
       {allGroupsOpen && (
         <AllGroupsStandingsModal
           open
