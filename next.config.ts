2b07d6f3587a57187be8fdefa783cefa6491ec5f
Hector <hhector7722@gmail.com>
2026-07-04 21:50:45 +0200

    ae

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["sharp"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "savsnkgpvvmdbaujqqoa.supabase.co",
+        pathname: "/storage/v1/object/public/stickers/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
