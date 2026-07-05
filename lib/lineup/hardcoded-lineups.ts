ef97f0af2b168eca1f8f77841dfb2785082599ab
Hector <hhector7722@gmail.com>
2026-07-06 00:55:11 +0200

    feat: add belgium, colombia, switzerland, morocco, and france to hardcoded lineups

--- lib/lineup/hardcoded-lineups.ts
+++ lib/lineup/hardcoded-lineups.ts
@@ -42,4 +42,40 @@ export const HARDCODED_DEFAULT_LINEUPS: Record<string, HardcodedLineup> = {
     formation: "4-3-3",
     startingNumbers: [24, 16, 3, 13, 5, 17, 4, 8, 2, 20, 10],
   },
+  bélgica: {
+    formation: "4-2-3-1",
+    startingNumbers: [1, 21, 4, 3, 5, 8, 20, 10, 7, 11, 17], // Courtois, Castagne, Mechele, Theate, De Cuyper, Tielemans, Vanaken, Trossard, De Bruyne, Doku, De Ketelaere
+  },
+  belgium: {
+    formation: "4-2-3-1",
+    startingNumbers: [1, 21, 4, 3, 5, 8, 20, 10, 7, 11, 17],
+  },
+  colombia: {
+    formation: "4-3-3",
+    startingNumbers: [12, 2, 23, 3, 17, 14, 16, 11, 10, 9, 7], // Vargas, Muñoz, Sanchez, Lucumi, Mojica, Puerta, Lerma, Arias, Rodriguez, Cordoba, Luis Diaz
+  },
+  suiza: {
+    formation: "4-2-3-1",
+    startingNumbers: [1, 6, 4, 5, 13, 8, 10, 11, 9, 17, 7], // Kobel, Zakaria, Elvedi, Akanji, Rodriguez, Freuler, Xhaka, Ndoye, Manzambi, Vargas, Embolo
+  },
+  switzerland: {
+    formation: "4-2-3-1",
+    startingNumbers: [1, 6, 4, 5, 13, 8, 10, 11, 9, 17, 7],
+  },
+  marruecos: {
+    formation: "4-2-3-1",
+    startingNumbers: [1, 2, 14, 25, 3, 24, 6, 10, 8, 23, 11], // Bono, Hakimi, Diop, Halhal, Mazraoui, El Aynaoui, Bouaddi, Brahim Diaz, Ounahi, El Khannouss, Saibari
+  },
+  morocco: {
+    formation: "4-2-3-1",
+    startingNumbers: [1, 2, 14, 25, 3, 24, 6, 10, 8, 23, 11],
+  },
+  francia: {
+    formation: "4-2-3-1",
+    startingNumbers: [16, 5, 4, 17, 3, 6, 14, 7, 11, 12, 10], // Maignan, Kounde, Upamecano, Saliba, Digne, Kone, Rabiot, Dembele, Olise, Barcola, Mbappe
+  },
+  france: {
+    formation: "4-2-3-1",
+    startingNumbers: [16, 5, 4, 17, 3, 6, 14, 7, 11, 12, 10],
+  },
 };
